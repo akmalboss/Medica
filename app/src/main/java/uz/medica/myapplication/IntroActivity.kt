0e1c4f5c145f22e95d41eda24afbe803cb2b2e60
Akmal <mamatovakmaljon@gmail.com>
2024-08-29 14:23:55 +0500

    29.08.2024 1

--- app/src/main/java/uz/medica/myapplication/IntroActivity.kt
+++ app/src/main/java/uz/medica/myapplication/IntroActivity.kt
@@ -1,20 +1,21 @@
 package uz.medica.myapplication
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import uz.medica.myapplication.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            startBtn.setOnClickListener {
+                startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+            }
         }
     }
 }
